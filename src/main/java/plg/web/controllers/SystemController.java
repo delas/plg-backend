d3bd99930bc11ae79bc7c15912b5120b9e96256b
Andrea Burattin <andrea.burattin@gmail.com>
2020-10-26 16:48:57 +0100

    Added system controller

---
+++ src/main/java/plg/web/controllers/SystemController.java
@@ -0,0 +1,16 @@
+package plg.web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2/system/")
+public class SystemController {
+
+	@GetMapping("/ping")
+	public @ResponseBody String ping() {
+		return "pong";
+	}
+}
